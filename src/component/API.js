2361e32603a000acdabf38023e54f523fbd401fc
Nguyendz102 <thenguyen9603@gmail.com>
2023-12-02 21:56:28 +0700

    all

---
+++ src/component/API.js
@@ -0,0 +1,7 @@
+export const URL = "http://localhost:8080"
+export const GET_SIZE = URL +"/api-admin/getSize"
+export const GET_ALL_CONTACT = URL +"/api-admin/contact"
+export const PUT_PHANHOI_LIENHE = URL +"/api-admin/put-phanhoi-contact"
+export const GET_THANH_TOAN = URL +"/api-admin/9pay"
+
+
